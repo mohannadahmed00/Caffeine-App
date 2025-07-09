d66967537a66c02b559ead9c59393a1d2148538b
Mohannad Ahmed <mohannad199344@gmail.com>
2025-07-09 10:51:59 +0300

    Refactor SnackScreen to use VerticalPager for snack selection; update UI components and add new string resource

--- app/src/main/java/com/giraffe/caffeineapp/screen/snack/SnackScreen.kt
+++ app/src/main/java/com/giraffe/caffeineapp/screen/snack/SnackScreen.kt
@@ -1,6 +1,5 @@
 package com.giraffe.caffeineapp.screen.snack
 
-import android.util.Log
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Image
@@ -9,45 +8,47 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.pager.VerticalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.giraffe.caffeineapp.R
 import com.giraffe.caffeineapp.ui.theme.CaffeineAppTheme
-import com.giraffe.caffeineapp.ui.theme.offWhite
+import com.giraffe.caffeineapp.ui.theme.darkGray
+import com.giraffe.caffeineapp.ui.theme.urbanist
 import com.giraffe.caffeineapp.ui.theme.white
 import kotlin.math.abs
 
 @Composable
-fun SnackScreen(modifier: Modifier = Modifier) {
+fun SnackScreen() {
     val snacks by rememberSaveable {
         mutableStateOf(
             listOf(
@@ -61,30 +62,10 @@ fun SnackScreen(modifier: Modifier = Modifier) {
         )
     }
 
-    var centeredItemIndex by rememberSaveable { mutableIntStateOf(0) }
-    val listState = rememberLazyListState()
+    val pagerState = rememberPagerState(0) { snacks.size }
 
-    val itemHeight = 250.dp
-    val spacing = 16.dp
     val screenHeight = LocalWindowInfo.current.containerSize.height.dp
-    val sidePadding = (screenHeight - itemHeight) / 2
 
-
-    LaunchedEffect(listState) {
-        snapshotFlow { listState.firstVisibleItemIndex to listState.firstVisibleItemScrollOffset }
-            .collect {
-                // Find which item is closest to the center
-                val layoutInfo = listState.layoutInfo
-                val viewportCenter = layoutInfo.viewportEndOffset / 2
-
-                val centeredItem = layoutInfo.visibleItemsInfo.minByOrNull { item ->
-                    val itemCenter = item.offset + item.size / 2
-                    abs(itemCenter - viewportCenter)
-                }
-
-                centeredItem?.let { centeredItemIndex = it.index }
-            }
-    }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -93,47 +74,67 @@ fun SnackScreen(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.Start,
         verticalArrangement = Arrangement.Center
     ) {
-        LazyColumn(
-            modifier = Modifier.fillMaxWidth(),
-            state = listState,
-            verticalArrangement = Arrangement.spacedBy(-50.dp),
-            contentPadding = PaddingValues(vertical = 250.dp)
-
-            ) {
-            itemsIndexed(snacks) { index, snack ->
-
-                val center = listState.layoutInfo.viewportEndOffset / 2
-                val itemOffset = listState.layoutInfo.visibleItemsInfo
-                    .find { it.index == index }
-                    ?.let { it.offset + it.size / 2 } ?: 0
-                val distanceFromCenterPercent = 1f - (abs(center - itemOffset) / center.toFloat())
+        Icon(
+            modifier = Modifier
+                .padding(28.dp)
+                .size(24.dp)
+                .align(Alignment.Start),
+            imageVector = Icons.Rounded.Close,
+            tint = darkGray.copy(.87f),
+            contentDescription = stringResource(R.string.close)
+        )
+        Text(
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .padding(bottom = 24.dp, top = 8.dp)
+                .fillMaxWidth(),
+            text = stringResource(R.string.take_your_snack),
+            style = TextStyle(
+                color = darkGray.copy(.87f),
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold,
+                letterSpacing = 0.25.sp,
+                fontFamily = urbanist,
+                textAlign = TextAlign.Start
+            )
+        )
+        Column(
+            modifier = Modifier.weight(1f),
+            verticalArrangement = Arrangement.Center
+        ) {
+            VerticalPager(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(1f),
+                state = pagerState,
+                pageSpacing = -screenHeight / 4,
+                contentPadding = PaddingValues(vertical = 50.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            )
+            { pageIndex ->
+                val isHero = pageIndex == pagerState.currentPage
                 val offsetX = animateDpAsState(
-                    if (index != centeredItemIndex) -(250 * (1f - distanceFromCenterPercent)).dp
-                    else (-20).dp
+                    if (isHero) (-100).dp else {
+                        if (abs(pageIndex - pagerState.currentPage) == 1) (-150).dp else (-550).dp
+                    }
                 ).value
-
                 val offsetZ = animateFloatAsState(
-                    if (index == centeredItemIndex) 0f
+                    if (isHero) 0f
                     else {
-                        if (index > centeredItemIndex) {
+                        if (pageIndex > pagerState.currentPage) {
                             30f
                         } else {
                             -30f
                         }
                     }
                 ).value
-
-                val offsetY = animateDpAsState(
-                    if (index > centeredItemIndex) (-50).dp else 0.dp
-                ).value
-
                 SnackCard(
                     modifier = Modifier
-                        .offset(x = offsetX, y = offsetY)
+                        .offset(x = offsetX)
                         .graphicsLayer {
                             rotationZ = offsetZ
                         },
-                    snack = snack, isIgnored = index < centeredItemIndex
+                    snack = snacks[pageIndex], isIgnored = pagerState.currentPage > pageIndex
                 )
             }
         }
